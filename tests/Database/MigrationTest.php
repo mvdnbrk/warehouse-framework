25e2ceff0d8c2913b420146848ba6af7d5a6aee0
Mark van den Broek <mvdnbrk@gmail.com>
2019-04-25 18:24:27 +0200

    Adds migration test.

---
+++ tests/Database/MigrationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Just\Warehouse\Tests\Database;
+
+use Just\Warehouse\Tests\TestCase;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MigrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_runs_the_locations_migration()
+    {
+        $columns = Schema::connection('warehouse')->getColumnListing('locations');
+
+        $this->assertEquals([
+            'id',
+            'name',
+            'created_at',
+            'updated_at',
+        ], $columns);
+    }
+
+    /** @test */
+    public function it_runs_the_inventories_migration()
+    {
+        $columns = Schema::connection('warehouse')->getColumnListing('inventories');
+
+        $this->assertEquals([
+            'id',
+            'location_id',
+            'gtin',
+            'created_at',
+            'updated_at',
+        ], $columns);
+    }
+}
