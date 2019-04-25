2aec7748d4b639542a691f1b729dc8cc0dbd54df
Mark van den Broek <mvdnbrk@gmail.com>
2019-04-25 18:20:14 +0200

    Adds locations migration.

---
+++ database/migrations/2019_04_01_000001_create_locations_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Get the migration connection name.
+     *
+     * @return string
+     */
+    public function getConnection()
+    {
+        return config('warehouse.database_connection');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 24);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
