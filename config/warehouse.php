b465bacec0461d2e4310ecd53ae39a8fa5b3f64e
Mark van den Broek <mvdnbrk@gmail.com>
2019-04-25 18:18:18 +0200

    Adds database_connection to config.

--- config/warehouse.php
+++ config/warehouse.php
@@ -2,4 +2,17 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Warehouse Database Connection
+    |--------------------------------------------------------------------------
+    |
+    | This is the database connection you want to use.
+    | By default we assume you have prepared a new
+    | connection called "warehouse". However, you
+    | may change this to anything you want.
+    |
+    */
+    'database_connection' => env('WAREHOUSE_DB_CONNECTION', 'warehouse'),
+
 ];
