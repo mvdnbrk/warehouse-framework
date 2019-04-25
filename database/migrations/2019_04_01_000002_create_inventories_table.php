449be1b6173ae3db33e1c467ba45b734ec2e709f
Mark van den Broek <mvdnbrk@gmail.com>
2019-04-25 18:20:27 +0200

    Adds inventories migration.

---
+++ database/migrations/2019_04_01_000002_create_inventories_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Get the migration connection name.
+     *
+     * @return string
+     */
+    public function getConnection()
+    {
+        return config('warehouse.database_connection');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('location_id');
+            $table->string('gtin', 14);
+            $table->timestamps();
+
+            $table->foreign('location_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
