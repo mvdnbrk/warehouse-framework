92258e52d3c664c70101d29984acb9829098474c
Mark van den Broek <mvdnbrk@gmail.com>
2019-04-25 18:23:44 +0200

    Setup the environment.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,22 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
-    //
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $config = $app->get('config');
+
+        $config->set('database.default', 'warehouse');
+
+        $config->set('database.connections.warehouse', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
 }
