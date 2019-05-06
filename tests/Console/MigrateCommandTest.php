5f7ba7231ebc1dbedb4f588b7a7690dfdee60d6f
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-06 21:52:36 +0200

    Renames test.

--- tests/Console/MigrateCommandTest.php
+++ tests/Console/MigrateCommandTest.php
@@ -4,7 +4,7 @@ namespace Just\Warehouse\Tests\Console;
 
 use Just\Warehouse\Tests\TestCase;
 
-class MigrateTest extends TestCase
+class MigrateCommandTest extends TestCase
 {
     /** @test */
     public function it_can_run_the_migration()
