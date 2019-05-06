585ed831dd0c0ce25d0e97ac1ba6f983d74a178d
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-06 21:33:09 +0200

    Register the install command.

--- src/WarehouseServiceProvider.php
+++ src/WarehouseServiceProvider.php
@@ -28,6 +28,7 @@ class WarehouseServiceProvider extends ServiceProvider
         );
 
         $this->commands([
+            Console\Commands\InstallCommand::class,
             Console\Commands\MigrateCommand::class,
             Console\Commands\MakeLocationCommand::class,
         ]);
