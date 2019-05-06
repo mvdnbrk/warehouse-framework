fa6bf3d362d9b3214815242723b336768bfab700
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-06 21:30:33 +0200

    Fixes typo.

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -11,7 +11,7 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'warehosue:install';
+    protected $signature = 'warehouse:install';
 
     /**
      * The console command description.
