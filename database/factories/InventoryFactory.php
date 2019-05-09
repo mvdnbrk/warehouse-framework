d714430c185d3a897d6502e375db2cfb0aea5171
Mark van den Broek <mvdnbrk@gmail.com>
2019-05-09 16:02:04 +0200

    Fixes typo.

--- database/factories/InventoryFactory.php
+++ database/factories/InventoryFactory.php
@@ -7,7 +7,7 @@ use Just\Warehouse\Models\Inventory;
 $factory->define(Inventory::class, function (Faker $faker) {
     return [
         'gtin' => $faker->ean13,
-        'location_id' =>  function () {
+        'location_id' => function () {
             return factory(Location::class)->create()->id;
         },
     ];
