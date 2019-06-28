6155634bda215616bdc4722153a52a4df16591d8
Mark van den Broek <mvdnbrk@gmail.com>
2019-06-28 16:20:34 +0200

    Updates docblock.

--- src/Contracts/StorableEntity.php
+++ src/Contracts/StorableEntity.php
@@ -9,7 +9,7 @@ interface StorableEntity
     /**
      * Move the inventory model to another location.
      *
-     * @param  Location  $location
+     * @param  \Just\Warehouse\Models\Location  $location
      * @return bool
      */
     public function move(Location $location);
