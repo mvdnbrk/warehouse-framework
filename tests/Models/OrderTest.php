015d2a21ec4b459a974c8a8df5b45776a27c9d11
Mark van den Broek <mvdnbrk@gmail.com>
2019-08-26 20:36:01 +0200

    Formatting.

--- tests/Models/OrderTest.php
+++ tests/Models/OrderTest.php
@@ -3,7 +3,6 @@
 namespace Just\Warehouse\Tests\Model;
 
 use LogicException;
-use Illuminate\Support\Carbon;
 use Just\Warehouse\Models\Order;
 use Just\Warehouse\Tests\TestCase;
 use Just\Warehouse\Models\Location;
@@ -134,65 +133,6 @@ class OrderTest extends TestCase
         }
 
         $this->fail('Adding an order line succeeded with an invalid gtin.');
-    }
-
-    /** @test */
-    public function it_can_be_processed()
-    {
-        $location = factory(Location::class)->create();
-        $location->addInventory('1300000000000');
-        $order = factory(Order::class)->create();
-        $order->addLine('1300000000000');
-
-        $order->process();
-
-        $this->assertEquals('open', $order->fresh()->status);
-    }
-
-    /** @test */
-    public function it_can_be_processed_with_unfilfilled_order_lines_which_results_in_status_backorder()
-    {
-        $order = factory(Order::class)->create();
-        $order->addLine('1300000000000');
-
-        $order->process();
-
-        $this->assertEquals('backorder', $order->fresh()->status);
-    }
-
-    /** @test */
-    public function it_can_be_marked_as_fulfilled()
-    {
-        Carbon::setTestNow('2019-10-11 12:34:56');
-        $location = factory(Location::class)->create();
-        $inventory = $location->addInventory('1300000000000');
-        $order = factory(Order::class)->create();
-        $order->addLine('1300000000000');
-        $order->process();
-
-        tap($order->fresh(), function ($order) {
-            $order->markAsFulfilled();
-            $this->assertEquals('fulfilled', $order->status);
-            $this->assertEquals('2019-10-11 12:34:56', $order->fulfilled_at);
-        });
-        $this->assertTrue($inventory->fresh()->trashed());
-    }
-
-    /** @test */
-    public function trying_to_mark_a_non_open_order_as_fulfilled_throws_an_exception()
-    {
-        $order = factory(Order::class)->create(['status' => 'created']);
-
-        try {
-            $order->markAsFulfilled();
-        } catch (LogicException $e) {
-            $this->assertEquals('This order can not be marked as fulfilled.', $e->getMessage());
-            $this->assertEquals('created', $order->fresh()->status);
-
-            return;
-        }
-
-        $this->fail('Trying to mark a non open order as fulfilled succeeded.');
     }
 
     /** @test */
