38b62b5e4acc23b5618deabecfa9b9740b9e9a1c
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-01 23:31:36 +0100

    Refactor.

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -25,8 +25,7 @@ use Spatie\ModelStates\HasStates;
 class Order extends AbstractModel
 {
     use HasStates,
-        SoftDeletes,
-        Concerns\ManagesPickList;
+        SoftDeletes;
 
     /**
      * The attributes that should be cast to native types.
@@ -133,5 +132,65 @@ class Order extends AbstractModel
     public function process()
     {
         TransitionOrderStatus::dispatch($this);
+    }
+
+    /**
+     * Determine if a pick list is available.
+     *
+     * @return bool
+     */
+    public function hasPickList()
+    {
+        return $this->status->is(Open::class);
+    }
+
+    /**
+     * Retrieve a picklist.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function pickList()
+    {
+        if (! $this->hasPickList()) {
+            return collect();
+        }
+
+        return $this->lines()
+            ->select('id')
+            ->with([
+                'inventory' => function ($query) {
+                    $query->select([
+                        'inventories.id',
+                        'inventories.gtin',
+                        'inventories.location_id',
+                    ]);
+                },
+                'inventory.location' => function ($query) {
+                    $query->select([
+                        'id',
+                        'name',
+                    ]);
+                },
+            ])
+            ->get()
+            ->map(function ($line) {
+                return $line->inventory->makeHidden([
+                    'id',
+                    'location_id',
+                ]);
+            })
+            ->groupBy([
+                'gtin',
+                'location.id',
+            ])
+            ->flatten(1)
+            ->map(function ($item) {
+                return collect($item->first())
+                    ->forget('location')
+                    ->put('location', $item->first()->location->name)
+                    ->put('quantity', $item->count());
+            })
+            ->sortBy('location')
+            ->values();
     }
 }
