3e9bd20e5f9a49c0e43e9157e17d2193528d1123
Mark van den Broek <mvdnbrk@gmail.com>
2020-03-22 19:56:50 +0100

    Remove docblock

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -6,11 +6,6 @@ use Exception;
 
 class ValidationException extends Exception
 {
-    /**
-     * Create a new exception instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct('The given data was invalid.');
