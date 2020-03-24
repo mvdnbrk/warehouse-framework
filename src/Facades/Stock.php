930a82d79d102e64c79b7add0780c2e7a22b721c
Mark van den Broek <mvdnbrk@gmail.com>
2020-03-24 21:49:09 +0100

    Remove docblock

--- src/Facades/Stock.php
+++ src/Facades/Stock.php
@@ -9,11 +9,6 @@ use Illuminate\Support\Facades\Facade;
  */
 class Stock extends Facade
 {
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
     protected static function getFacadeAccessor(): string
     {
         return 'stock';
