3ece256f94a56ad033b73700d91d5e1f44e81e7d
Mark van den Broek <mvdnbrk@gmail.com>
2020-03-24 21:49:01 +0100

    Add return type declaration

--- src/Facades/Stock.php
+++ src/Facades/Stock.php
@@ -14,7 +14,7 @@ class Stock extends Facade
      *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'stock';
     }
