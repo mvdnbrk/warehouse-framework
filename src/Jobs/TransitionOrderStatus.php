18248e3f079e174a5decf0fb65098dccdcd81164
Mark van den Broek <mvdnbrk@gmail.com>
2020-09-22 11:08:35 +0200

    Use `discardExpiration` method

--- src/Jobs/TransitionOrderStatus.php
+++ src/Jobs/TransitionOrderStatus.php
@@ -45,7 +45,7 @@ class TransitionOrderStatus implements ShouldQueue
             return;
         }
 
-        $this->order->setExpiresAtAttribute(0);
+        $this->order->discardExpiration();
         $this->order->status->transitionTo($this->newStatus);
     }
 }
